d7c17a22f93ef0c1ff6e5a7b9232fda5b8258a50
Shehroz Irfan <>
2022-10-11 23:33:55 +0500

    Class Attributes

--- classes/animal.rb
+++ classes/animal.rb
@@ -1,17 +1,35 @@
 class Animal
   attr_accessor :noise, :color
 
+  # class attributes
+  @@species = ['Cow', 'Goat', 'Camel', 'horse', 'cat']
+  # class attributes start with @@
+  # remember the class attributes are shared among the instances of the class
+  @@totalAnimalsCount = 0
+  @@totalAnimalsList = []
+
+  # initialize is an instance method
   def initialize(options={})
     @noise = options[:noise]
     @color = options[:color]
+
+    # we want the @@totalAnimalCount to be incremented whenever there is an instace of this class. As
+    # the class attributes are shared so, the value of @@totalAnimalCount will be incremented every time
+    # whenever an instance of this class is created
+    @@totalAnimalsCount += 1
+    @@totalAnimalsList << self
   end
 
   # class methods
   def self.types
-    ['Cow', 'Goat', 'Camel']
+    @@species
   end
 
   def self.create_a_cat
     Animal.new({noise: "Meowww!", color: "White"})
+  end
+
+  def self.total_animal_count
+    @@totalAnimalsCount
   end
 end
--- init.rb
+++ init.rb
@@ -2,13 +2,26 @@
 
 require_relative 'classes/animal'
 
-# looping through the animal types, remember 'types' is a class method.
-Animal.types.each do |type|
-  puts type
-end
-
 puts "-------"
-
+puts "Creating Tam..."
 tam = Animal.create_a_cat
 puts tam.noise
 puts tam.color
+
+puts "-------"
+
+puts "Total Animal Count: "
+puts Animal.total_animal_count
+
+puts "-------"
+puts "Creating Jam..."
+jam = Animal.new({noise: "Bark!", color: "Black"})
+puts jam.noise
+puts jam.color
+
+puts "-------"
+
+puts "Total Animal Count: "
+puts Animal.total_animal_count
+
+puts "-------"
