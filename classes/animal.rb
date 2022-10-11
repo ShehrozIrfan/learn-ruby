d15d2a3a12b360bff15b03918412c3be6095d434
Shehroz Irfan <>
2022-10-11 23:40:45 +0500

    Class reader and writer methods - we define our own reader and writer

--- classes/animal.rb
+++ classes/animal.rb
@@ -1,35 +1,20 @@
 class Animal
-  attr_accessor :noise, :color
+  # in ruby till now we don't have class reader and writer methods like attr_*
+  # but in rails we've these as cattr_*
+  # in ruby so far we don't have
+  # we can make our own reader and writer methods for class attributes
 
-  # class attributes
-  @@species = ['Cow', 'Goat', 'Camel', 'horse', 'cat']
-  # class attributes start with @@
-  # remember the class attributes are shared among the instances of the class
-  @@totalAnimalsCount = 0
-  @@totalAnimalsList = []
+  @@species = ['Cat', 'Dog', 'Horse', 'Snake']
 
-  # initialize is an instance method
-  def initialize(options={})
-    @noise = options[:noise]
-    @color = options[:color]
-
-    # we want the @@totalAnimalCount to be incremented whenever there is an instace of this class. As
-    # the class attributes are shared so, the value of @@totalAnimalCount will be incremented every time
-    # whenever an instance of this class is created
-    @@totalAnimalsCount += 1
-    @@totalAnimalsList << self
-  end
-
-  # class methods
-  def self.types
+  # class attribute reader
+  def self.species
     @@species
   end
 
-  def self.create_a_cat
-    Animal.new({noise: "Meowww!", color: "White"})
+  # class attribute writer
+  def self.species=(array)
+    return unless array.is_a?(Array)
+    @@species = array
   end
 
-  def self.total_animal_count
-    @@totalAnimalsCount
-  end
 end
--- init.rb
+++ init.rb
@@ -2,26 +2,9 @@
 
 require_relative 'classes/animal'
 
-puts "-------"
-puts "Creating Tam..."
-tam = Animal.create_a_cat
-puts tam.noise
-puts tam.color
-
-puts "-------"
-
-puts "Total Animal Count: "
-puts Animal.total_animal_count
-
-puts "-------"
-puts "Creating Jam..."
-jam = Animal.new({noise: "Bark!", color: "Black"})
-puts jam.noise
-puts jam.color
-
-puts "-------"
-
-puts "Total Animal Count: "
-puts Animal.total_animal_count
-
-puts "-------"
+puts '------'
+puts Animal.species
+puts '------'
+Animal.species = ['Goat', 'Cow', 'Camel']
+puts Animal.species
+puts '------'
