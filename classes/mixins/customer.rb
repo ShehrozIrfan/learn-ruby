aa5177bfea4d9c9298d0968e2f16c330939cbac8
Shehroz Irfan <>
2022-10-12 13:01:04 +0500

    Include is only used for including modules

--- classes/mixins/customer.rb
+++ classes/mixins/customer.rb
@@ -2,6 +2,7 @@ require_relative 'nameable'
 require_relative 'location_info'
 
 class Customer
+  # Note: In ruby, include is only used for including modules
   include Nameable
   include LocationInfo
 end
